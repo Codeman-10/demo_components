ff9944408514dbae35f40f337deafdbf4a2267e8
gokul <you@example.com>
2021-06-02 11:07:01 +0530

    react-table

--- src/index.js
+++ src/index.js
@@ -1,112 +1,115 @@
 import React from "react";
 import { render } from "react-dom";
-import { makeData, Logo, Tips } from "./Utils";
-import Stepper from 'react-stepper-horizontal';
+import { makeData } from "./Utils";
+
 // Import React Table
 import ReactTable from "react-table";
 import "react-table/react-table.css";
+
+const columns = [
+  {
+    Header: "Name",
+    columns: [
+      {
+        Header: "First Name",
+        accessor: "firstName"
+      },
+      {
+        Header: "Last Name",
+        id: "lastName",
+        accessor: (d) => d.lastName
+      }
+    ]
+  },
+  {
+    Header: "Info",
+    columns: [
+      {
+        Header: "Age",
+        accessor: "age"
+      },
+      {
+        Header: "Status",
+        accessor: "status"
+      }
+    ]
+  },
+  {
+    Header: "Stats",
+    columns: [
+      {
+        Header: "Visits",
+        accessor: "visits"
+      }
+    ]
+  }
+];
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       data: makeData(),
-      dummydata: [{
-        firstName: "gokul",
-        lastName: "Vijay"
-      }, {
-        firstName: "gokul",
-        lastName: "Vijay"
-      }, {
-        firstName: "gokul",
-        lastName: "Vijay"
-      }]
-
+      expanded: {}
     };
   }
-
-  renderCell(row) {
-
-    console.log(row)
-    return (<div className="test1">
-      <div className="test2">
-        <Stepper steps={[{ title: 'Step One' }, { title: 'Step Two' }, { title: 'Step Three' }, { title: 'Step Four' }]} activeStep={1} />
-
-      </div>
-      <ReactTable
-        data={this.state.dummydata}
-        columns={[
-          {
-            Header: "First Name",
-            accessor: "firstName",
-          },
-          {
-            Header: "Last Name",
-            accessor: "lastName",
-          },
-          {
-
-            Header: "Age",
-            accessor: "age",
-          }
-        ]}
-        showPagination={false}
-        sortable={false}
-        defaultPageSize={3}
-        className="-striped -highlight"
-      />
-    </div>);
-  }
-
   render() {
     const { data } = this.state;
     return (
       <div>
         <ReactTable
-
+          expanded={this.state.expanded}
+          onExpandedChange={(expandedList) => {
+            const expanded = {};
+            Object.keys(expandedList).forEach((item) => {
+              const expand = Boolean(
+                expandedList[item] && expandedList[item].constructor === Object
+              );
+              expanded[item] = expand;
+            });
+            this.setState({ expanded });
+          }}
+          getTrProps={(state, rowInfo, instance) => {
+            let onject = this.state.expanded;
+            var index = Object.keys(onject).filter(function (key) { return onject[key] === true })[0];
+            console.log(index,rowInfo.index)
+            return {
+              style: {
+                border: rowInfo.index == index
+                    ? "solid 1px red"
+                    : "none"
+              }
+            }
+          }}
           data={data}
-          columns={[
-            {
-              Header: "First Name",
-              accessor: "firstName",
-            },
-            {
-              Header: "Last Name",
-              accessor: "lastName",
-            },
-            {
-
-              Header: "Age",
-              accessor: "age",
-            }
-            ,
-
-            {
-              columns: [
-                {
-                  expander: true,
-                  Header: () => <strong>More</strong>,
-                  width: 65,
-                  Expander: ({ isExpanded, ...rest }) =>
-                    <div>
-                      {isExpanded
-                        ? <span>close</span>
-                        : <span>open</span>}
-                    </div>,
-                  style: {
-                    cursor: "pointer",
-                    fontSize: 25,
-                    padding: "0",
-                    textAlign: "center",
-                    userSelect: "none"
-                  },
-                }
-              ]
-            }
-          ]}
+          columns={columns}
           defaultPageSize={10}
           className="-striped -highlight"
-          SubComponent={(row) => this.renderCell(row)}
+          SubComponent={(row) => {
+            return (
+              <div style={{ padding: "20px" ,border:"1px solid red" }}>
+                <em>
+                  You can put any component you want here, even another React
+                  Table!
+                </em>
+                <br />
+                <br />
+                <ReactTable
+                  data={data}
+                  columns={columns}
+                  defaultPageSize={3}
+                  showPagination={false}
+                  SubComponent={(row) => {
+                    return (
+                      <div style={{ padding: "20px" }}>
+                        Another Sub Component!
+                      </div>
+                    );
+                  }}
+                />
+              </div>
+            );
+          }}
         />
         <br />
 
